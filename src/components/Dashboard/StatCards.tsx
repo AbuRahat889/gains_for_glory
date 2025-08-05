d0c9a2020a06b352d7cc0627ee74234adfae3734
AbuRahat889 <146001390+AbuRahat889@users.noreply.github.com>
2025-08-05 16:28:04 +0600

    refactor: streamline Dashboard and StatCards components for improved data handling and layout

--- src/components/Dashboard/StatCards.tsx
+++ src/components/Dashboard/StatCards.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import wallet from "@/assets/icon/wallet.svg";
 import user from "@/assets/icon/star.svg";
 import Image from "next/image";
-import { HiOutlineArrowNarrowUp } from "react-icons/hi";
 // Stat Card Component
 interface StatCardProps {
   icon: string;
@@ -26,15 +25,8 @@ function StatCard({ icon, title, value }: StatCardProps) {
           <p className="text-textColor text-base font-medium">{title}</p>
           <div className="flex flex-row items-center lg:items-start xl:items-center gap-3">
             <p className="text-2xl md:text-4xl font-semibold text-secondaryColor mt-2">
-              12,540
+             {value}
             </p>
-
-            <div className="bg-[#f8f7ff] w-16 md:w-24 h-8 md:h-10 flex  gap-2 items-center justify-center border-2 border-[#7b61ff] rounded-3xl">
-              <HiOutlineArrowNarrowUp className="text-[#7b61ff] h-16 md:h-24" />
-              <p className="text-[#7b61ff] text-xs md:text-sm font-medium leading-normal">
-                100%
-              </p>
-            </div>
           </div>
         </div>
       </div>
@@ -43,13 +35,12 @@ function StatCard({ icon, title, value }: StatCardProps) {
 }
 
 interface StatCardsProps {
-  totalBooking: number | string | null;
-  totalRevenue: number | string | null;
+  totalEarning: number | string | null;
   totalUser: number | string | null;
 }
 
 const StatCards = ({
-  totalBooking,
+  totalEarning,
 
   totalUser,
 }: StatCardsProps) => {
@@ -57,13 +48,13 @@ const StatCards = ({
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-6">
       <StatCard
         icon={wallet}
-        title="Total Users"
+        title="Total User"
         value={totalUser?.toString() || "N/A"}
       />
       <StatCard
         icon={user}
-        title="Total Bookings"
-        value={totalBooking?.toString() || "N/A"}
+        title="Total Earning"
+        value={totalEarning?.toString() || "N/A"}
       />
     </div>
   );
